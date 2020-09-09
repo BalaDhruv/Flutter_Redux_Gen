c1b5d93ce4a9e61a6c4cdfd5908f994f91839d6c
Balamurugan <androbalamail@gmail.com>
2020-09-09 01:01:02 -0700

    video and code updated.

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,49 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import { CREATE_STATE_PLACE_HOLDER, NAME_ERROR_MESSAGE, NAME_REG_EXP, DEFAULT_NAME, STATE_EXTENSION } from './resources/constants';
 
-const EXTENSION = '.dart';
-const STATE_EXTENSION = '.state.dart';
-const REDUCER_EXTENSION = '.state.dart';
-const MIDDLEWARE_EXTENSION = '.state.dart';
-const ACTION_EXTENSION = '.state.dart';
-const DEFAULT_NAME = 'default';
-const CREATE_STATE_PLACE_HOLDER = 'Enter State Name';
-const NAME_ERROR_MESSAGE = 'Please use name without space. Consider using "_"';
-const NAME_REG_EXP = new RegExp(/[`0-9 ~!@#$%^&*()|+\-=?;:'",.<>\{\}\[\]\\\/]/);
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "flutter-redux-gen" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	// Create State Command Registering
 	let createState = vscode.commands.registerCommand('flutter-redux-gen.createState', (args) => {
-		// The code you place here will be executed every time your command is executed
 
 		let focusedFilePath = getFilePath(args.path);
 		let nameField = vscode.window.createInputBox();
+		let nameFieldValidator = new RegExp(NAME_REG_EXP);
 		nameField.placeholder = CREATE_STATE_PLACE_HOLDER;
 		nameField.onDidChangeValue((v) => {
-			console.log(NAME_REG_EXP.test(v));
-			nameField.validationMessage = NAME_REG_EXP.test(v)? NAME_ERROR_MESSAGE : '';
+			nameField.validationMessage = nameFieldValidator.test(v)? NAME_ERROR_MESSAGE : '';
 		});
 		nameField.onDidAccept(() => {
-			if (nameField.value.includes(' ')) {
+			if (nameFieldValidator.test(nameField.value)) {
+				nameField.validationMessage = NAME_ERROR_MESSAGE;
+			} else {
+				nameField.hide();
+				var name = nameField.value ? nameField.value : DEFAULT_NAME;
+				createFile(focusedFilePath, name, STATE_EXTENSION);
+				nameField.validationMessage = '';
+			}
+		});
+		nameField.show();
+	});
+
+	context.subscriptions.push(createState);
+
+	// Create State Command Registering
+	let createReducer = vscode.commands.registerCommand('flutter-redux-gen.createReducer', (args) => {
+
+		let focusedFilePath = getFilePath(args.path);
+		let nameField = vscode.window.createInputBox();
+		let nameFieldValidator = new RegExp(NAME_REG_EXP);
+		nameField.placeholder = CREATE_STATE_PLACE_HOLDER;
+		nameField.onDidChangeValue((v) => {
+			nameField.validationMessage = nameFieldValidator.test(v)? NAME_ERROR_MESSAGE : '';
+		});
+		nameField.onDidAccept(() => {
+			if (nameFieldValidator.test(nameField.value)) {
 				nameField.validationMessage = NAME_ERROR_MESSAGE;
 			} else {
 				nameField.hide();
@@ -57,13 +65,17 @@ function getFilePath(path: string) {
 function createFile(fPath: string, name: string, extention: string) {
 	const pathWithFileName = fPath + '/' + name.toLocaleLowerCase() + extention;
 	fs.writeFile(pathWithFileName, getStateGenCode(name), err => {
-		vscode.window.showInformationMessage('Please check your path. Otherwise file a issue in Git Repo. Let me help.');
+		console.log(err);
+		if(err){
+			vscode.window.showInformationMessage('Please check your path. Otherwise file a issue in Git Repo. Let me help.');
+		}else{
+			vscode.window.showInformationMessage('State Created Successfully.');
+		}
 	});
-	vscode.window.showInformationMessage('State Created Successfully.');
 }
 
 function getStateGenCode(stateName: string) {
-	const sName = formattedName(stateName);
+	const sName = getFormattedStateName(stateName);
 	return `
 class ${sName}State {
 	final bool loading;
@@ -94,7 +106,7 @@ class ${sName}State {
 	  `;
 }
 
-function formattedName(name:string) {
+function getFormattedStateName(name:string) {
 	return  name.split('_').map(word=>word[0].toLocaleUpperCase()+word.substring(1)).join('');
 }
 
